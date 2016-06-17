b131c21c9d85b23a81917f6a1fbac5ff8c8e0a12
Irham Iqbal <irham@Irhams-MacBook-Pro.local>
2016-06-17 16:40:57 +0530

    file to resart the servers

---
+++ restart_server.sh
@@ -0,0 +1,3 @@
+systemctl restart kube-apiserver.service                                               
+systemctl restart kube-controller-manager.service                                            
+systemctl restart kube-scheduler.service
